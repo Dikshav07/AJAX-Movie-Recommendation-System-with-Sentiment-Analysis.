c1fb7d8b6dbaef11b6fe2de38a767083f38cbae2
Diksha Verma <150647851+Dikshav07@users.noreply.github.com>
2024-09-26 23:50:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,14 @@ import urllib.request
 import pickle
 import requests
 
-# load the nlp model and tfidf vectorizer from disk
 filename = 'nlp_model.pkl'
 clf = pickle.load(open(filename, 'rb'))
 vectorizer = pickle.load(open('tranform.pkl','rb'))
 
 def create_similarity():
     data = pd.read_csv('main_data.csv')
-    # creating a count matrix
     cv = CountVectorizer()
     count_matrix = cv.fit_transform(data['comb'])
-    # creating a similarity score matrix
     similarity = cosine_similarity(count_matrix)
     return data,similarity
 
