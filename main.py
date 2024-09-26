5355ebac02deaee517c696de84d734a245e1bb82
Diksha Verma <150647851+Dikshav07@users.noreply.github.com>
2024-09-26 23:50:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,14 +33,13 @@ def rcmd(m):
         i = data.loc[data['movie_title']==m].index[0]
         lst = list(enumerate(similarity[i]))
         lst = sorted(lst, key = lambda x:x[1] ,reverse=True)
-        lst = lst[1:11] # excluding first item since it is the requested movie itself
+        lst = lst[1:11] 
         l = []
         for i in range(len(lst)):
             a = lst[i][0]
             l.append(data['movie_title'][a])
         return l
     
-# converting list of string to list (eg. "["abc","def"]" to ["abc","def"])
 def convert_to_list(my_list):
     my_list = my_list.split('","')
     my_list[0] = my_list[0].replace('["','')
